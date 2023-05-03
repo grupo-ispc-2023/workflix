8a9a426904d56409dfc042edcf8736f6ea6ab1dc
Andrea-Cal <andiesc83@hotmail.com>
2023-05-03 01:09:33 -0300

    Se agrega el archivo UsuarioClienteService.py dentro del modulo Service

---
+++ BACKEND/Service/UsuarioClienteService.py
@@ -0,0 +1,19 @@
+from typing import List
+from entities.usuario_profesional_entity import UsuarioProfesionalEntity
+from entities.usuario_cliente_entity import UsuarioClienteEntity
+
+
+class UsuarioClienteService:
+    def buscarProfesionales(self) -> List[UsuarioProfesional]:
+        # Código para buscar y retornar los usuarios profesionales
+        lista_profesionales = []
+        # Código para obtener la lista de usuarios profesionales
+        return lista_profesionales
+        
+    def contratar(self, usuario: UsuarioProfesional) -> None:
+        # Código para realizar la contratación del usuario profesional especificado
+        pass
+        
+    def valorarProfesional(self, usuario: UsuarioProfesional, valoracion: int, comentario: str) -> None:
+        # Código para agregar una valoración y comentario al usuario profesional especificado
+        pass
