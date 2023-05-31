64486b3107814d59ec586855ced509d211f8c361
marceloedelgado <marceloedelgado@gmail.com>
2023-05-31 17:29:13 -0300

    se resuelve el modo 'active' cuando se presiona un boton de filtro de profesion cambios en profesionales.component HTML y TS

--- src/app/layouts/pages/profesionales/profesionales.component.ts
+++ src/app/layouts/pages/profesionales/profesionales.component.ts
@@ -1,49 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { Tarjeta } from 'src/assets/datos-profesionales/tarjetas';
 import { ApiService } from 'src/app/service/api.service';
 import { CarritoService } from 'src/app/service/carrito.service';
 
 @Component({
   selector: 'app-profesionales',
   templateUrl: './profesionales.component.html',
-  styleUrls: ['./profesionales.component.css'],
+  styleUrls: ['./profesionales.component.css']
 })
 export class ProfesionalesComponent implements OnInit {
   public profesionalesList: any[] = [];
   public filterProfesion: any[] = [];
-  searchKey: string = '';
-  lista: Tarjeta[] = [];
+  public searchKey: string = '';
+  public filtroActivo: string = '';
 
-  constructor(private api: ApiService, private carritoService: CarritoService) {}
+  constructor(private api: ApiService, private carritoService: CarritoService) { }
 
   ngOnInit(): void {
-    this.api.getProfesional().subscribe((res: any[]) => {
-      this.profesionalesList = res.map((item: any) => ({
-        ...item,
-        quantity: 1,
-        total: item.price,
-      }));
-
-      this.filterProfesion = [...this.profesionalesList];
-      this.lista = [...this.profesionalesList];
+    this.api.getProfesional().subscribe(res => {
+      this.profesionalesList = res;
+      this.filterProfesion = res;
     });
-
     this.carritoService.search.subscribe((val: any) => {
       this.searchKey = val;
     });
   }
 
-  agregarcarrito(item: any) {
+  agregarcarrito(item: any): void {
     this.carritoService.agregarCarrito(item);
   }
 
-  filter(profesion: string) {
-    if (profesion === '') {
-      this.filterProfesion = [...this.profesionalesList];
-    } else {
-      this.filterProfesion = this.profesionalesList.filter(
-        (item: any) => item.profesion === profesion
-      );
-    }
+  filter(profesion: string): void {
+    this.filterProfesion = this.profesionalesList.filter((a: any) => {
+      if (a.profesion === profesion || profesion === '') {
+        return a;
+      }
+    });
+    this.filtroActivo = profesion; // Actualiza el filtro activo
   }
 }
